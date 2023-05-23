480354ffe59d2a8e7d1a00bb675fd48d750bbe94
Grégory <greg.marini@hotmail.fr>
2023-05-23 12:16:46 +0200

    start build auth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",    
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#27374D',
+        secondary: '#526D82',
+        tertiary: '#9DB2BF',
+        genBg: '#DDE6ED',
+      },
+      width: {
+        contWithXl: '1240px',
+      },
+    },
   },
   plugins: [],
-}
-
+};
