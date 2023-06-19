8b15f1ccf95f58d78d83a2d5800e8a81aaf2438b
Grégory Marini <greg.marini@hotmail.fr>
2023-06-19 22:00:44 +0200

    secret in auth options

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -23,6 +23,7 @@ export async function getUserFromMongoDB(email) {
 }
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
